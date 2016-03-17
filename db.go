a5fd9eeaa3ad22e87f987cb5732c579f2d7c7881
rizkisunaryo <rizkisunaryo@gmail.com>
2016-03-17 20:53:51 +0700

    add QueryLog function

--- db.go
+++ db.go
@@ -11,8 +11,18 @@ func ExecLog(db *sql.DB, sqlStr string, args ...interface{}) (sql.Result,string,
 		return nil,sqlStr,err
 	}
 	res,err := stmt.Exec(args...)
+
+	return res, SqlLog(sqlStr,args...), err
+}
+
+func QueryLog(db *sql.DB, sqlStr string, args ...interface{}) (*sql.Rows, string, error) {
+	rows,err := db.Query(sqlStr, args...)
+	return rows, SqlLog(sqlStr,args...), err
+}
+
+func SqlLog(sqlStr string, args ...interface{}) string {
 	for _, e := range args {
 		sqlStr = strings.Replace(sqlStr, "?", "'"+fmt.Sprintf("%v",e)+"'", 1)
 	}
-	return res,sqlStr,err
+	return sqlStr
 }
